7b604e85f5a85d41376f0c4adb9cbf599050625a
DavidGelmanSerg <gelmands@college.omsu.ru>
2023-06-15 22:50:23 +0600

    Add files via upload

---
+++ DB_MANAGER.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using CONFIGURATOR;
+
+namespace DB_MANAGER
+{
+    public class DB_Manager
+    {
+        //methods
+
+        //открыть подкючение
+        public void OpenConnection(MySql.Data.MySqlClient.MySqlConnection connection, Configurator config)
+        {
+            if(connection.State.ToString() == "Open") connection.Close();
+
+            string ConnectionString = $"server={config.IP_settings.Replace(" ", "")};uid={config.User_id_Settings}; pwd={config.Password_Settings};database={config.DB_Settings};Connection Timeout=3";
+            connection.ConnectionString = ConnectionString;
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Подключение успешно!");
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show("Ошибка подключения!\n" + ex.Message);
+            }
+        }
+        //получить информацию о пассажирах
+        public DataTable GetPassengersInfo(MySql.Data.MySqlClient.MySqlConnection connection)
+        {
+            DataTable passengers_table = new DataTable();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandType = CommandType.TableDirect;
+            cmd.CommandText = "flight_company.Passengers";
+            cmd.Connection = connection;
+
+            MySqlDataReader row_set = cmd.ExecuteReader();
+
+            for(int i = 0; i<row_set.FieldCount;i++)
+            {
+                passengers_table.Columns.Add(row_set.GetName(i));
+            }
+            while(row_set.Read())
+            {
+                string[] data_string = new string[row_set.FieldCount];  
+                for(int i = 0;i < row_set.FieldCount;i++)
+                {
+                    data_string[i] = row_set[i].ToString();
+                }
+                passengers_table.Rows.Add(data_string);
+            }
+            row_set.Close();
+            return passengers_table;
+        }
+
+        public DataTable GetFlightsInfo (MySql.Data.MySqlClient.MySqlConnection connection)
+        {
+            DataTable fligths_table = new DataTable();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandType = CommandType.TableDirect;
+            cmd.CommandText = "flight_company.flight_schedule";
+            cmd.Connection = connection;
+
+            MySqlDataReader row_set = cmd.ExecuteReader();
+
+            for (int i = 0; i < row_set.FieldCount; i++)
+            {
+                fligths_table.Columns.Add(row_set.GetName(i));
+            }
+            while (row_set.Read())
+            {
+                string[] data_string = new string[row_set.FieldCount];
+                for (int i = 0; i < row_set.FieldCount; i++)
+                {
+                    data_string[i] = row_set[i].ToString();
+                }
+                fligths_table.Rows.Add(data_string);
+            }
+            row_set.Close();
+
+            return fligths_table;
+        }
+
+        public DataTable GetArchiveInfo(MySql.Data.MySqlClient.MySqlConnection connection)
+        {
+            DataTable archive_table = new DataTable();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandType = CommandType.TableDirect;
+            cmd.CommandText = "flight_company.Archive";
+            cmd.Connection = connection;
+
+            MySqlDataReader row_set = cmd.ExecuteReader();
+
+            for (int i = 0; i < row_set.FieldCount; i++)
+            {
+                archive_table.Columns.Add(row_set.GetName(i));
+            }
+            while (row_set.Read())
+            {
+                string[] data_string = new string[row_set.FieldCount];
+                for (int i = 0; i < row_set.FieldCount; i++)
+                {
+                    data_string[i] = row_set[i].ToString();
+                }
+                archive_table.Rows.Add(data_string);
+            }
+            row_set.Close();
+
+
+            return archive_table;
+        }
+
+        public void AddPassenger(string name, string sname, string patr, string passport_number, string start_point, string end_point, string seat, string date_time, MySql.Data.MySqlClient.MySqlConnection connection)
+        {
+            /*Алгоритм:
+             * Проверить, существует ли запись с таким пассажиром:
+             * [
+             *      ЕСЛИ ДА, то 
+             *      {
+             *              INSERT данные в промежуточную таблицу
+             *              UPDATE количество мест для этого рейса
+             *      }
+             *      ЕСЛИ НЕТ, то
+             *      {
+             *              INSERT запись в таблицу пассажиров
+             *              INSERT данные в промежуточную таблицу
+             *              UPDATE количество мест для этого рейса
+             *      }
+             * ]
+             */
+            String addPassenger = $"use flight_company; " +
+    $"INSERT INTO Passengers (passport_number, first_name, second_name, patronymic, seat) " +
+    $"VALUES ({passport_number}, {name},{sname},{patr},{seat});\r\n " +
+    $"INSERT INTO flight_schedule_has_passengers VALUES(" +
+            $"(SELECT DISTINCT FLIGHT_NUMBER from flight_schedule where date_time = '{date_time}' and start_point = '{start_point}' and end_point = '{end_point}'), " +
+            $"{date_time}, {start_point}, {end_point}, (SELECT COUNT(*) from Passengers));";
+
+
+            try
+            {
+                MySqlCommand addPassengerCmd = new MySqlCommand(addPassenger, connection);
+                addPassengerCmd.ExecuteNonQuery();
+                MessageBox.Show("Пассажир успешно добавлен");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+                MessageBox.Show(addPassenger);
+            }
+
+          
+        }
+
+        public void UpdatePassenger(string name, string sname, string patr, string passport_number, MySql.Data.MySqlClient.MySqlConnection connection)
+        {
+
+        }
+
+        public void DeletePassenger(string name, string sname, string patr, string passport_number, string start_point, string end_point, string date_time, MySql.Data.MySqlClient.MySqlConnection connection)
+        {
+
+        }
+    }
+}
---
+++ GUI_Factory.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_FACTORY
+{
+    public class GUI_Factory
+    {
+
+        /*------------------------------------------------------------------------------------------methods------------------------------------------------------------------------------------------*/
+        //создание кнопки
+        public Button CreateButton(string button_name, int x, int y, int width, int height, Control Parent_Element) 
+        {
+            Button button = new Button();
+            button.Name = button_name;
+            button.Text = button_name;
+            button.Location = new System.Drawing.Point(x, y);
+            button.Size = new System.Drawing.Size(width, height);
+            Parent_Element.Controls.Add(button);
+            return button;
+        }
+
+        //создание поля ввода
+        public TextBox CreateTextBox(string TextBox_name, string text, int x, int y, int width, int height, bool multiline, Control Parent_Element)
+        {
+            TextBox textBox = new TextBox();
+            textBox.Name = TextBox_name;
+            textBox.Text = text;
+            textBox.Location = new System.Drawing.Point(x, y);
+            textBox.Size = new System.Drawing.Size(width, height);
+            textBox.Multiline = multiline;
+            Parent_Element.Controls.Add(textBox);
+            return textBox;
+        }
+
+        //создание элемента Лейбл
+        public Label CreateLabel(string text, int x, int y, Control Parent_Element, bool autosize)
+        {
+            Label label = new Label();
+            label.Location = new System.Drawing.Point(x, y);
+            label.AutoSize = autosize;
+            label.Text = text;
+            Parent_Element.Controls.Add(label);
+            return label;
+        }
+
+        //создать Таблицу
+        public DataGridView CreateDataGridView(int x, int y, int width, int height, bool rw_state, Control Parent_Element, DataTable table = null)
+        {
+            DataGridView dataGridView = new DataGridView();
+            dataGridView.Location = new System.Drawing.Point(x, y);
+            dataGridView.Size = new System.Drawing.Size(width, height);
+            dataGridView.ReadOnly= rw_state;
+            dataGridView.DataSource = table;
+            Parent_Element.Controls.Add(dataGridView);
+            return dataGridView;
+        }
+
+        //создать список 
+        public ListView CreateListView (int x, int y, int width, int height, View view_type, Control Parent_Element)
+        {
+            ListView list = new ListView();
+            list.Location = new System.Drawing.Point(x, y);
+            list.Size = new System.Drawing.Size(width, height);
+            list.View = view_type;
+            Parent_Element.Controls.Add(list);
+
+            return list;
+        }
+
+        //создать контейнер
+        public GroupBox CreateGroupBox (string text, int x, int y, int width, int height, Control Parent_Element)
+        {
+            GroupBox groupBox = new GroupBox();
+            groupBox.Text = text;
+            groupBox.Location = new System.Drawing.Point(x, y);
+            groupBox.Size = new System.Drawing.Size(width, height);
+            Parent_Element.Controls.Add(groupBox);
+            return groupBox;
+        }
+        /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    }
+}
